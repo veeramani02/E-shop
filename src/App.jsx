8f1982b529bf6722879fe098f82fff4f60a8369f
veeramani <veeramaniveer64@gmail.com>
2026-01-19 18:02:03 +0530

    fix cart import path case issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Shop from "./pages/Shop"
-import Cart from "./pages/cart"
+import Cart from './pages/Cart'
 import Checkout from "./pages/Checkout"
 import { useState } from "react"
 import Order from "./pages/Order"
